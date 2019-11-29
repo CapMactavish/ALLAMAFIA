77c1c2419cc96d996103fe1b9d2c62cfa5972414
Amir <ahamjadi@gmail.com>
2019-11-29 16:43:15 +0330

    change in citizen

--- src/roles/Citizen.java
+++ src/roles/Citizen.java
@@ -1,5 +1,10 @@
 package roles;
 
 public class Citizen extends Person{
+    protected static int iterator = 1;
 
+    public Citizen() {
+        super.id += iterator;
+        iterator++;
+    }
 }
