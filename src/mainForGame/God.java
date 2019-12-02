5c97ab815d5b72530d9a26a8ed5303b3b34cea7d
unknown <ah.saroo@yahoo.com>
2019-12-02 21:16:12 +0330

    fixing initiation method

--- src/mainForGame/God.java
+++ src/mainForGame/God.java
@@ -1,6 +1,12 @@
 package mainForGame;
 
-import roles.*;
+import roles.Citizen;
+import roles.Detective;
+import roles.Doctor;
+import roles.GodFather;
+import roles.Mafia;
+import roles.Natasha;
+import roles.Person;
 
 public class God {
 	public boolean dayOrNight = true;
@@ -14,42 +20,27 @@ public class God {
 		int a1, a2;
 		a1 = (int) a / 3;
 		a2 = a - a1;
+		int j = 0;
 		int[] temp = new int[8];
 		boolean[] choises = new boolean[21];
-		for (int i = 0; i < temp.length; i++) {
-			if (i == 0) {
-				temp[i] = (int) (Math.random() * 1000) % 20;
-				choises[temp[i]] = true;
+		while(true) {
+			if (j == 0) {
+				temp[j] = (int) (Math.random() * 1000) % 20;
+				choises[temp[j]] = true;
+				j++;
+				
 			} else {
 				while (true) {
-					if (temp[i] != temp[i - 1] && choises[i] == false)
-						temp[i] = (int) (Math.random() * 1000) % 20;
+					if (temp[j] != temp[j - 1] && choises[temp[j]] == false)
+						temp[j] = (int) (Math.random() * 1000) % 20;
 					{
-						choises[i] = true;
+						choises[temp[j]] = true;
+						j++;
 						break;
 					}
 				}
 
 			}
-		}
-		for (int i = 0; i < temp.length; i++) {
-			System.out.print(temp[i] + "\t");
-		}
-
-		for (int i = 0; i < gamers.length; i++) {
-			gamers[temp[0]] = new GodFather();
-			gamers[temp[1]] = new Natasha();
-			gamers[temp[2]] = new Doctor();
-			gamers[temp[3]] = new Detective();
-			gamers[temp[4]] = new Mafia();
-			gamers[temp[5]] = new Mafia();
-			gamers[temp[6]] = new Mafia();
-			gamers[temp[7]] = new Mafia();
-
-		}
-		for (int i = 0; i < gamers.length; i++) {
-			if (gamers[i] == null)
-				gamers[i] = new Citizen();
 		}
 
 	}
