3703971c58cc964756daad59dc5be6ba51a92f0b
unknown <ah.saroo@yahoo.com>
2019-12-03 22:01:20 +0330

    creating a find method in detective

--- src/roles/Detective.java
+++ src/roles/Detective.java
@@ -1,5 +1,12 @@
 package roles;
 
-public class Detective extends Person{
+import java.util.ArrayList;
 
+public class Detective extends Person{
+	public boolean findMafia(ArrayList<Integer> a) {
+		int temp = (int) (Math.random() * 1000) % (a.size());
+		if (temp %2 == 0)
+			return true;
+		return false;		
+	}
 }
